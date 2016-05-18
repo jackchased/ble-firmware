678210932df0e7b8a7320db057c9d7d2a7a6bc27
Jack Wu <jackchased@gmail.com>
2016-05-18 17:46:09 +0800

    add RelayModule profiles

---
+++ RelayModule/Profiles/powerservice.h
@@ -0,0 +1,133 @@
+/**************************************************************************************************
+  Filename:       powerservice.h
+  Revised:        $Date: 2013-08-23 11:45:31 -0700 (Fri, 23 Aug 2013) $
+  Revision:       $Revision: 35100 $
+
+  Description:    Power and Current service definitions and prototypes
+
+
+  Copyright 2012-2013  Texas Instruments Incorporated. All rights reserved.
+
+  IMPORTANT: Your use of this Software is limited to those specific rights
+  granted under the terms of a software license agreement between the user
+  who downloaded the software, his/her employer (which must be your employer)
+  and Texas Instruments Incorporated (the "License").  You may not use this
+  Software unless you agree to abide by the terms of the License. The License
+  limits your use, and you acknowledge, that the Software may not be modified,
+  copied or distributed unless embedded on a Texas Instruments microcontroller
+  or used solely and exclusively in conjunction with a Texas Instruments radio
+  frequency transceiver, which is integrated into your product.  Other than for
+  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+  works of, modify, distribute, perform, display or sell this Software and/or
+  its documentation for any purpose.
+
+  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+  Should you have any questions regarding your right to use this Software,
+  contact Texas Instruments Incorporated at www.TI.com.
+**************************************************************************************************/
+
+#ifndef POWERSERVICE_H
+#define POWERSERVICE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "st_util.h"
+  
+/*********************************************************************
+ * CONSTANTS
+ */
+
+// Service UUID
+#define POWER_SERV_UUID             0xBB40
+#define POWER_DATA_UUID             0xBB41
+#define CURRENT_DATA_UUID           0xBB42
+#define POWER_CONF_UUID             0xBB43
+#define POWER_PERI_UUID             0xBB44
+
+// Sensor Profile Services bit fields
+#define POWER_SERVICE               0x00000001
+
+// Length of sensor data in bytes
+#define POWER_DATA_LEN              4
+#define CURRENT_DATA_LEN            2
+
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*********************************************************************
+ * MACROS
+ */
+
+
+/*********************************************************************
+ * API FUNCTIONS
+ */
+
+
+/*
+ * Power_AddService- Initializes the Sensor GATT Profile service by registering
+ *          GATT attributes with the GATT server.
+ *
+ * @param   services - services to add. This is a bit map and can
+ *                     contain more than one service.
+ */
+extern bStatus_t Power_AddService( uint32 services );
+
+/*
+ * Power_RegisterAppCBs - Registers the application callback function.
+ *                    Only call this function once.
+ *
+ *    appCallbacks - pointer to application callbacks.
+ */
+extern bStatus_t Power_RegisterAppCBs( sensorCBs_t *appCallbacks );
+
+/*
+ * Power_SetParameter - Set a Sensor GATT Profile parameter.
+ *
+ *    param - Profile parameter ID
+ *    len   - length of data to write
+ *    value - pointer to data to write.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ */
+extern bStatus_t Power_SetParameter( uint8 param, uint8 len, void *value );
+
+/*
+ * Power_GetParameter - Get a Sensor GATT Profile parameter.
+ *
+ *    param - Profile parameter ID
+ *    value - pointer to data to write.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ */
+extern bStatus_t Power_GetParameter( uint8 param, void *value );
+
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* POWERSERVICE_H */
---
+++ RelayModule/Profiles/relayservice.h
@@ -0,0 +1,133 @@
+/**************************************************************************************************
+  Filename:       relayservice.h
+  Revised:        $Date: 2013-08-23 11:45:31 -0700 (Fri, 23 Aug 2013) $
+  Revision:       $Revision: 35100 $
+
+  Description:    Relay service definitions and prototypes
+
+
+  Copyright 2012-2013 Texas Instruments Incorporated. All rights reserved.
+
+  IMPORTANT: Your use of this Software is limited to those specific rights
+  granted under the terms of a software license agreement between the user
+  who downloaded the software, his/her employer (which must be your employer)
+  and Texas Instruments Incorporated (the "License").  You may not use this
+  Software unless you agree to abide by the terms of the License. The License
+  limits your use, and you acknowledge, that the Software may not be modified,
+  copied or distributed unless embedded on a Texas Instruments microcontroller
+  or used solely and exclusively in conjunction with a Texas Instruments radio
+  frequency transceiver, which is integrated into your product.  Other than for
+  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+  works of, modify, distribute, perform, display or sell this Software and/or
+  its documentation for any purpose.
+
+  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+  Should you have any questions regarding your right to use this Software,
+  contact Texas Instruments Incorporated at www.TI.com.
+**************************************************************************************************/
+
+#ifndef RELAYSERVICE_H
+#define RELAYSERVICE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "st_util.h"
+  
+/*********************************************************************
+ * CONSTANTS
+ */
+
+// Service UUID
+#define RELAY_SERV_UUID          0xBB50
+#define RELAY_DATA_UUID          0xBB51
+#define RELAY_CONF_UUID          0xBB52
+#define RELAY_PERI_UUID          0xBB53
+
+// Sensor Profile Services bit fields
+#define RELAY_SERVICE            0x00000002
+
+// Length of sensor data in bytes
+#define RELAY_DATA_LEN           1
+
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * Profile Callbacks
+ */
+
+/*********************************************************************
+ * API FUNCTIONS
+ */
+
+/*
+ * Relay_AddService- Initializes the Sensor GATT Profile service by registering
+ *          GATT attributes with the GATT server.
+ *
+ * @param   services - services to add. This is a bit map and can
+ *                     contain more than one service.
+ */
+extern bStatus_t Relay_AddService( uint32 services );
+
+/*
+ * Relay_RegisterAppCBs - Registers the application callback function.
+ *                    Only call this function once.
+ *
+ *    appCallbacks - pointer to application callbacks.
+ */
+extern bStatus_t Relay_RegisterAppCBs( sensorCBs_t *appCallbacks );
+
+/*
+ * Relay_SetParameter - Set a Sensor GATT Profile parameter.
+ *
+ *    param - Profile parameter ID
+ *    len - length of data to right
+ *    value - pointer to data to write.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ */
+extern bStatus_t Relay_SetParameter( uint8 param, uint8 len, void *value );
+
+/*
+ * Relay_GetParameter - Get a Sensor GATT Profile parameter.
+ *
+ *    param - Profile parameter ID
+ *    value - pointer to data to write.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ */
+extern bStatus_t Relay_GetParameter( uint8 param, void *value );
+
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RELAYSERVICE_H */
