e1e49187b742d2ee1df0dbe1bb6dfdc06ff97809
Jack Wu <jackchased@gmail.com>
2016-05-24 15:22:44 +0800

    add GasShtSensor profiles

---
+++ GasShtSensor/Profiles/buzzerservice.h
@@ -0,0 +1,133 @@
+/**************************************************************************************************
+  Filename:       buzzerservice.h
+  Revised:        $Date: 2013-08-23 11:45:31 -0700 (Fri, 23 Aug 2013) $
+  Revision:       $Revision: 35100 $
+
+  Description:    Buzzer service definitions and prototypes
+
+
+  Copyright 2012-2013 Texas Instruments Incorporated. All rights reserved.
+
+  IMPORTANT: Your use of this Software is limited to those specific rights
+  granted under the terms of a software license agreement between the user
+  who downloaded the software, his/her employer (which must be your employer)
+  and Texas Instruments Incorporated (the "License").  You may not use this
+  Software unless you agree to abide by the terms of the License. The License
+  limits your use, and you acknowledge, that the Software may not be modified,
+  copied or distributed unless embedded on a Texas Instruments microcontroller
+  or used solely and exclusively in conjunction with a Texas Instruments radio
+  frequency transceiver, which is integrated into your product.  Other than for
+  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+  works of, modify, distribute, perform, display or sell this Software and/or
+  its documentation for any purpose.
+
+  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+  Should you have any questions regarding your right to use this Software,
+  contact Texas Instruments Incorporated at www.TI.com.
+**************************************************************************************************/
+
+#ifndef BUZZERSERVICE_H
+#define BUZZERSERVICE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "st_util.h"
+  
+/*********************************************************************
+ * CONSTANTS
+ */
+
+// Service UUID
+#define BUZZER_SERV_UUID          0xBB80
+#define BUZZER_DATA_UUID          0xBB81
+#define BUZZER_CONF_UUID          0xBB82
+#define BUZZER_PERI_UUID          0xBB83
+
+// Sensor Profile Services bit fields
+#define BUZZER_SERVICE            0x00000008
+
+// Length of sensor data in bytes
+#define BUZZER_DATA_LEN           1
+
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * Profile Callbacks
+ */
+
+/*********************************************************************
+ * API FUNCTIONS
+ */
+
+/*
+ * Buzzer_AddService- Initializes the Sensor GATT Profile service by registering
+ *          GATT attributes with the GATT server.
+ *
+ * @param   services - services to add. This is a bit map and can
+ *                     contain more than one service.
+ */
+extern bStatus_t Buzzer_AddService( uint32 services );
+
+/*
+ * Buzzer_RegisterAppCBs - Registers the application callback function.
+ *                    Only call this function once.
+ *
+ *    appCallbacks - pointer to application callbacks.
+ */
+extern bStatus_t Buzzer_RegisterAppCBs( sensorCBs_t *appCallbacks );
+
+/*
+ * Buzzer_SetParameter - Set a Sensor GATT Profile parameter.
+ *
+ *    param - Profile parameter ID
+ *    len - length of data to right
+ *    value - pointer to data to write.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ */
+extern bStatus_t Buzzer_SetParameter( uint8 param, uint8 len, void *value );
+
+/*
+ * Buzzer_GetParameter - Get a Sensor GATT Profile parameter.
+ *
+ *    param - Profile parameter ID
+ *    value - pointer to data to write.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ */
+extern bStatus_t Buzzer_GetParameter( uint8 param, void *value );
+
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BUZZERSERVICE_H */
---
+++ GasShtSensor/Profiles/environmentalservice.h
@@ -0,0 +1,137 @@
+/**************************************************************************************************
+  Filename:       environmentalservice.h
+  Revised:        $Date: 2013-08-23 11:45:31 -0700 (Fri, 23 Aug 2013) $
+  Revision:       $Revision: 35100 $
+
+  Description:    Environmental Sensing service definitions and prototypes
+
+
+  Copyright 2012-2013 Texas Instruments Incorporated. All rights reserved.
+
+  IMPORTANT: Your use of this Software is limited to those specific rights
+  granted under the terms of a software license agreement between the user
+  who downloaded the software, his/her employer (which must be your employer)
+  and Texas Instruments Incorporated (the "License").  You may not use this
+  Software unless you agree to abide by the terms of the License. The License
+  limits your use, and you acknowledge, that the Software may not be modified,
+  copied or distributed unless embedded on a Texas Instruments microcontroller
+  or used solely and exclusively in conjunction with a Texas Instruments radio
+  frequency transceiver, which is integrated into your product.  Other than for
+  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+  works of, modify, distribute, perform, display or sell this Software and/or
+  its documentation for any purpose.
+
+  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+  Should you have any questions regarding your right to use this Software,
+  contact Texas Instruments Incorporated at www.TI.com.
+**************************************************************************************************/
+
+#ifndef ENVIRONMENTALSERVICE_H
+#define ENVIRONMENTALSERVICE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "st_util.h"
+  
+/*********************************************************************
+ * CONSTANTS
+ */
+
+// Service UUID
+#define ENVIRONMENTAL_SERV_UUID         0x181A
+#define TEMP_DATA_UUID                  0x2A6E
+#define HUMI_DATA_UUID                  0x2A6F
+#define TEMP_CONF_UUID                  0xBB63
+#define TEMP_PERI_UUID                  0xBB64
+
+// Sensor Profile Services bit fields
+#define ENVIRONMENTAL_SERVICE           0x00000008
+
+// Length of sensor data in bytes
+#define SHT20_DATA_LEN                  4
+#define TEMP_DATA_LEN                   2
+#define HUMI_DATA_LEN                   2
+  
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * Profile Callbacks
+ */
+
+
+  /*********************************************************************
+ * API FUNCTIONS
+ */
+
+/*
+ * Environmental_AddService- Initializes the Sensor GATT Profile service by registering
+ *          GATT attributes with the GATT server.
+ *
+ * @param   services - services to add. This is a bit map and can
+ *                     contain more than one service.
+ */
+extern bStatus_t Environmental_AddService( uint32 services );
+
+/*
+ * Environmental_RegisterAppCBs - Registers the application callback function.
+ *                    Only call this function once.
+ *
+ *    appCallbacks - pointer to application callbacks.
+ */
+extern bStatus_t Environmental_RegisterAppCBs( sensorCBs_t *appCallbacks );
+
+/*
+ * Environmental_SetParameter - Set a Sensor GATT Profile parameter.
+ *
+ *    param - Profile parameter ID
+ *    len - length of data to right
+ *    value - pointer to data to write.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ */
+extern bStatus_t Environmental_SetParameter( uint8 param, uint8 len, void *value );
+
+/*
+ * Environmental_GetParameter - Get a Sensor GATT Profile parameter.
+ *
+ *    param - Profile parameter ID
+ *    value - pointer to data to write.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ */
+extern bStatus_t Environmental_GetParameter( uint8 param, void *value );
+
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ENVIRONMENTALSERVICE_H */
